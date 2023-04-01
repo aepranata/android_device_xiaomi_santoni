65997fdd0e09da25d1cd4689353fa75844b76c92
Aditya Eka Pranata <aepranata@gmail.com>
2023-04-01 09:15:03 +0700

    santoni: Remove prebuilt package
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- device.mk
+++ device.mk
@@ -513,10 +513,6 @@ PRODUCT_PACKAGES += \
     XiaomiAddon \
     XiaomiParts
 
-#Prebuilts
-PRODUCT_PACKAGES += \
-    RemovePackages
-
 # Reduce system image size by limiting java debug info.
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 
